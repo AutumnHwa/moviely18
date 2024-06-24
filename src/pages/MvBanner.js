2fa496d164aa764046a6bca502af96d971a0418f
autumn <autumn@autumnui-MacBookPro.local>
2024-06-24 14:56:07 +0900

    id 20

--- src/pages/MvBanner.js
+++ src/pages/MvBanner.js
@@ -15,18 +15,19 @@ const flatrateLogos = {
   'wavve': wavveLogo
 };
 
-const MvBanner = ({ title, poster, flatrate, movieId, userId }) => {
+const MvBanner = ({ title, poster, flatrate, movieId }) => {
   const [rating, setRating] = useState(0);
   const [showModal, setShowModal] = useState(false);
   const [message, setMessage] = useState('');
   const navigate = useNavigate();
+  const userId = 20; // 사용자 ID를 하드코딩
 
   const handleStarClick = async (index) => {
     const newRating = index + 1;
     setRating(newRating);
 
     const ratingData = {
-      user_id: userId, // 사용자 ID를 props로 받음
+      user_id: userId, // 하드코딩된 사용자 ID 사용
       movie_id: movieId, // 영화 ID
       rating: parseFloat(newRating)
     };
